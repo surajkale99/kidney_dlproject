5b96d4e969f5c36d621a9f8c3525356b66a1293e
Suraj Kale <surajkale99@gmail.com>
2025-05-05 18:07:50 +0530

    data ingestion

--- main.py
+++ main.py
@@ -1,2 +1,18 @@
+
 from src.cnnClassifier import logger
-logger.info("Welocome to info")
+from src.cnnClassifier.utils.comman import get_size
+from src.cnnClassifier.entity.config import DataIngestionConfig
+from src.cnnClassifier.config.configuration import ConfigurationManager
+from src.cnnClassifier.components.data_ingestion_01 import DataIngestion
+from src.cnnClassifier.pipeline.data_ingestion import DataIngestionTrainingPipeline
+
+
+STAGE_NAME = "Data Ingestion stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+   logger.exception(e)
+   raise e 
---
+++ src/cnnClassifier/pipeline/data_ingestion.py
@@ -0,0 +1,38 @@
+import sys
+import os
+sys.path.append(os.getcwd()) 
+
+
+from src.cnnClassifier import logger
+from src.cnnClassifier.utils.comman import get_size
+from src.cnnClassifier.entity.config import DataIngestionConfig
+from src.cnnClassifier.config.configuration import ConfigurationManager
+from src.cnnClassifier.components.data_ingestion_01 import DataIngestion
+
+STAGE_NAME = "Data Ingestion stage"
+
+
+class DataIngestionTrainingPipeline:
+    def __init__(self):
+        pass
+
+    def main(self):
+        config = ConfigurationManager()
+        data_ingestion_config = config.get_data_ingestion_config()
+        data_ingestion = DataIngestion(config=data_ingestion_config)
+        data_ingestion.download_file()
+        data_ingestion.extract_zip_file()
+
+
+
+if __name__ == '__main__':
+    try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = DataIngestionTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
+
+
